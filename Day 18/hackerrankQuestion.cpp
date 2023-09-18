fe1dddc5d7d08c2c5196f5f0a24fb39e75a2ec34
Adarsh121003 <adarshtiwari.110044@gmail.com>
2023-09-18 07:00:12 +0530

    Day 18

---
+++ Day 18/hackerrankQuestion.cpp
@@ -0,0 +1,92 @@
+// Objective
+// Today, we're working with binary numbers. Check out the Tutorial tab for learning materials and an instructional video!
+
+// Task
+// Given a base- integer, , convert it to binary (base-). Then find and print the base- integer denoting the maximum number of consecutive 's in 's binary representation. When working with different bases, it is common to show the base as a subscript.
+
+// Example
+
+// The binary representation of  is . In base , there are  and  consecutive ones in two groups. Print the maximum, .
+
+// Input Format
+
+// A single integer, .
+
+// Constraints
+
+// Output Format
+
+// Print a single base- integer that denotes the maximum number of consecutive 's in the binary representation of .
+
+// Sample Input 1
+
+// 5
+// Sample Output 1
+
+// 1
+// Sample Input 2
+
+// 13
+// Sample Output 2
+
+// 2
+// Explanation
+
+// Sample Case 1:
+// The binary representation of  is , so the maximum number of consecutive 's is .
+
+// Sample Case 2:
+// The binary representation of  is , so the maximum number of consecutive 's is .
+// ==========================Solution===========================================
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+int main() {
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    int maxConsecutiveOnes = 0;
+    int currentConsecutiveOnes = 0;
+
+    while (n > 0) {
+        if (n % 2 == 1) {
+            currentConsecutiveOnes++;
+            maxConsecutiveOnes = max(maxConsecutiveOnes, currentConsecutiveOnes);
+        } else {
+            currentConsecutiveOnes = 0;
+        }
+        n /= 2;
+    }
+
+    cout << maxConsecutiveOnes << endl;
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
