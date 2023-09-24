737bde8a798c9513046ab46a8e2477f7804cbde8
Adarsh121003 <adarshtiwari.110044@gmail.com>
2023-09-24 14:36:19 +0530

    Day 24

---
+++ Day 24/hackerRank.cpp
@@ -0,0 +1,54 @@
+// Objective
+// Today, we're getting started with Exceptions by learning how to parse an integer from a string and print a custom error message. Check out the Tutorial tab for learning materials and an instructional video!
+
+// Task
+// Read a string, , and print its integer value; if  cannot be converted to an integer, print Bad String.
+
+// Note: You must use the String-to-Integer and exception handling constructs built into your submission language. If you attempt to use loops/conditional statements, you will get a  score.
+
+// Input Format
+
+// A single string, .
+
+// Constraints
+
+// , where  is the length of string .
+//  is composed of either lowercase letters () or decimal digits ().
+// Output Format
+
+// Print the parsed integer value of , or Bad String if  cannot be converted to an integer.
+
+// Sample Input 0
+
+// 3
+// Sample Output 0
+
+// 3
+// Sample Input 1
+
+// za
+// Sample Output 1
+
+// Bad String
+// Explanation
+
+// Sample Case  contains an integer, so it should not raise an exception when we attempt to convert it to an integer. Thus, we print the .
+// Sample Case  does not contain any integers, so an attempt to convert it to an integer will raise an exception. Thus, our exception handler prints Bad String.
+// ========================Solution============================================
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    string S;
+    getline(cin, S);
+
+    try {
+        int n = stoi(S);
+        cout << n << endl;
+    } catch (const invalid_argument& e) {
+        cout << "Bad String" << endl;
+    }
+
+    return 0;
+}
